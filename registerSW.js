4206b9761bc61321b9a9ffb1218d9f738f9eb7a0
rmitchellPitzer <rmitchellPitzer@users.noreply.github.com>
2024-12-06 02:49:20 +0000

    deploy: 51e9f210610f90662f4d0d8f06d63205c7b1cb7e

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/terminal/sw.js', { scope: '/terminal/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
